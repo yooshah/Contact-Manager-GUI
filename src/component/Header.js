fead7426851498c1b950d4eb398ddc3d263d5f95
yooshah <yooshahkk@gmail.com>
2023-02-18 22:55:08 +0530

    Contact manager

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div className="uo fixed menu">
+            <div className="ui container menu centered grid">
+                <h2>Contact Manager</h2>
+
+            </div>
+        </div>
+    );
+};
+export default Header;
